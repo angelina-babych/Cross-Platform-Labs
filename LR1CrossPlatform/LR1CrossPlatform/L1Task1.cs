552ea01bbfbff9529ab150e7623b96ff3a4f5de1
Ангеліна Бабич <115937394+angelina-babych@users.noreply.github.com>
2023-03-13 12:46:02 +0200

    Add files via upload

---
+++ LR1CrossPlatform/LR1CrossPlatform/L1Task1.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int n;
+int i = 0;
+int sum = 0;
+
+Console.Write("Enter the number of array elements:");
+if (!Int32.TryParse(Console.ReadLine(), out n))
+    n = 10;
+Console.WriteLine($"Array will have {n} elements!");
+int[] myArray = new int[n];
+Console.WriteLine();
+
+Random random = new Random();
+for (i = 0; i < n; i++)
+{
+    myArray[i] = random.Next(-100, 100);
+    Console.Write($"\t{myArray[i]}");
+}
+Console.WriteLine();
+
+i = 0;
+while (myArray[i] >= 0 && i < n)
+{
+    sum = sum + myArray[i];
+    i++;
+}
+Console.Write($"\nSum from the first element to first negative is: {sum}\n");
+
+
