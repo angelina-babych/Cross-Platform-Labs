55494944a4cfd99eb2677ca34271bd37515512b9
Ангеліна Бабич <115937394+angelina-babych@users.noreply.github.com>
2023-03-13 12:44:54 +0200

    Add files via upload

---
+++ LR1CrossPlatform/L1Task2/L1Task2.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Enter the line of text:");
+String? s = Console.ReadLine();
+if (string.IsNullOrEmpty(s))
+    s = "The Cat in the Hat is Not a Cat";
+Console.WriteLine();
+s = s.ToLower();
+Console.WriteLine($"\t{s}");
+
+for (int i = 0; i < s.Length; i++)
+{
+
+    if (s.IndexOf(s[i], s.IndexOf(s[i]) + 1) == -1)
+    {
+        Console.Write($"\nFirst non-repeating character is {s[i]}\n");
+
+        break;
+    }
+}
+
+
+
